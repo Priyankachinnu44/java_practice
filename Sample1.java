49ec6623ec41160532192cd3c84485cbd7febe10
Priyankachinnu44 <115424900+Priyankachinnu44@users.noreply.github.com>
2022-10-17 18:26:18 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,11 @@
+
+  class Sample1
+  {
+    public static void main(String args[])
+    {
+      int num1, num2, sum;
+      num1=20; num2=10;
+      sum=num1-num2;
+      System.out.println(sum);
+    }
+  }
