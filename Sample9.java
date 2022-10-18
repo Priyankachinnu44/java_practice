999d65990d3d35df70262d80f7f39737a1e35801
Priyankachinnu44 <115424900+Priyankachinnu44@users.noreply.github.com>
2022-10-18 09:16:54 +0530

    Add files via upload

---
+++ Sample9.java
@@ -0,0 +1,9 @@
+class Sample9
+  {
+    public static void main(String args[])
+    {
+     int a=6, b=5,c=2,peri;
+       peri=a+b+c;
+      System.out.println("perimeter of square:"+peri);
+    }
+  }
